681fe35dbf8688d8af55c1963ada1e9824d4823b
PallaviMali24 <malipallavi586@gmail.com>
2024-01-19 14:56:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ sr.reveal('.featured-name',{delay: 100})
 sr.reveal('.featured-text-info',{delay: 200})
 sr.reveal('.featured-text-btn',{delay: 200})
 sr.reveal('.social_icons',{delay: 200})
-sr.reveal('.featured-image',{delay: 300})
+sr.reveal('.featured-image',{delay:100})
 
 
 /* -- PROJECT BOX -- */
